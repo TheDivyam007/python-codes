e9d2395cf9a1b9144e9ed52f3badeac09736da09
Divyam <dk0864@srmist.edu.in>
2023-10-19 19:51:24 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,20 @@
+#1. Write a python program to calculate the sum of Two numbers and Three numbers.
+#However, if the sum is between 120 to 320 it will return 200
+
+#For 2 numbers
+a_030 = 10
+b_030 = 20
+
+sum_030 = a_030 + b_030
+if sum_030>=120 and sum_030<=320:
+    print("200")
+else:
+    print(sum_030)
+    
+#For 3 numbers
+c_030 = 100
+sum_030 = sum_030+c_030
+if sum_030>=120 and sum_030<=320:
+    print("200")
+else:
+    print(sum_030)
