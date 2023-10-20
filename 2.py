aa081451db085c4c57d2d952bbe150906c8e9878
Divyam <dk0864@srmist.edu.in>
2023-10-20 20:26:16 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,14 @@
+#2. Implement a python function to find the Maximum of Three numbers.
+
+a_030 = 10
+b_030 = 20
+c_030 = 50
+
+if a_030>b_030 and a_030>c_030:
+    print(a_030, "is maximum among the three")
+
+elif b_030>c_030:
+    print(b_030,"is maximum among the three")
+    
+else :
+    print(c_030,"is maximum among the three")
