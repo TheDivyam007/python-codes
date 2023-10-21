efc0d67e365aac4b8d79f7aa32b38e5fef498086
Divyam <dk0864@srmist.edu.in>
2023-10-21 20:30:56 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,10 @@
+#3. Write a python program to calculate the Factorial of a given number.
+
+num_0030 = 5
+factorial_0030 = 1
+
+while num_0030!=1:
+    factorial_0030 = factorial_0030*num_0030
+    num_0030 = num_0030-1
+    
+print(factorial_0030)
