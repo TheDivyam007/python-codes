266f45a34f7c67851e48be831404955aff5e2385
Divyam <dk0864@srmist.edu.in>
2023-10-28 19:19:50 +0530

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,16 @@
+#7. Write a python program to calculate the area and perimeter of different geometric
+#shapes (circle, rectangle, triangle, etc.).
+
+a_0030 = 10
+b_0030 = 20
+area_Triangle_0030 = (int)(1*a_0030*b_0030)/2
+print("Area of triangle : ",area_Triangle_0030)
+
+r_0030 = 10
+area_circle_0030 = 3.14*r_0030*r_0030
+print("Area of circle is : ", area_circle_0030)
+
+l_0030 = 10
+w_0030 = 5
+area_Rectangle_0030 = l_0030*w_0030
+print("Area of Rectangle : ", area_Rectangle_0030)
